fdac5559b9503bb88c04f4085bc187dfac38ac97
pabloRnt <pab.rena15@gmail.com>
2026-03-19 11:42:34 -0300

    aula 02 variáveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,31 @@
+print("Hello World!")
+
+print(7+4)
+print("7+4")
+print("7" + "4") #Concatenando Strings
+
+''' Comentários de 
+    múltiplas 
+    linhas
+'''
+
+#VARIÁVEIS
+nome = "Pablo"
+idade = 18
+peso = 65.3
+
+print("Bah \n", nome, idade, peso)
+print(f"Olá, {nome}!!")
+
+nome = input("Digite seu mome: ")
+idade = int(input ("Digite sua idade: "))
+peso = float(input("Digite seu peso"))
+
+print(nome, idade, peso)
+print (idade + 1)
+
+ano_nascimento = 2007
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+
+print(f"Sua idade é: {idade}")
