1fa7cfcfb6eb984826f179347e7610ec9af3ac43
pablinho-programador <pab.rena15@gmail.com>
2026-04-30 21:50:19 -0300

    Teste: atualização da main branch

--- aula05/aula05-exercicios/exercicio1.py
+++ aula05/aula05-exercicios/exercicio1.py
@@ -1,5 +1,5 @@
 '''EXERCÍCIO 1: Escreva um algoritmo que recebe um número inteiro n > 0, cria um vetor de números reais com n
-posições e preenche o vetor com n números aleatórios reais. '''
+posições e preenche o vetor com n números aleatórios reais.'''
 
 import random
 
